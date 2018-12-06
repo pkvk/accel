e6f6b832d028381cad82dcf8583a2e6e5e4dbf4d
BP <masterdezign@gmail.com>
2018-12-06 16:29:24 +0100

    Add matrix-vector product example from Accelerate documentation

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,23 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+
 module Main where
 
 import Lib
 import Prelude hiding ( zipWith )
-import Data.Array.Accelerate
-import Data.Array.Accelerate.LLVM.PTX
+import Data.Array.Accelerate             as A
+import Data.Array.Accelerate.LLVM.PTX    as A
 
 dotp :: Acc (Vector Float) -> Acc (Vector Float) -> Acc (Scalar Float)
 dotp xs ys = fold (+) 0 (zipWith (*) xs ys)
+
+-- | Matrix-vector product
+mvm :: A.Num a => Acc (Matrix a) -> Acc (Vector a) -> Acc (Vector a)
+mvm mat vec =
+  let Z :. rows :. cols = unlift (shape mat) :: Z :. Exp Int :. Exp Int
+      vec'              = A.replicate (lift (Z :. rows :. All)) vec
+  in
+  A.fold (+) 0 ( A.zipWith (*) mat vec' )
 
 main :: IO ()
 main = someFunc
